6be8a3d05e0f7822200f5798acf3501b950da863
TiebertLefebure <tiebert.lefebure@gmail.com>
2025-10-29 21:36:51 +0100

    simplified model (no inductance)

--- assignment1_solution.m
+++ assignment1_solution.m
@@ -130,11 +130,11 @@ theta = A\b
 
 % % alternative way to construct transfer function
 z = tf('z', Ts);
-sys_d1 = (theta(3)*z + theta(4))/(z^3 + theta(1)*z^2 + theta(2)*z);
+sys_d1 = (theta(3)*z + theta(4))/(z^3 + theta(1)*z^2 + theta(2)*z)
 
 % plot the results
 
-timeVectorToPlot = 0.01:0.01:28; % IN SECONDS (total time period = 42 s --- sampling period = 0.01 s)
+timeVectorToPlot = 0.01:0.01:28; % IN SECONDS (total time period = 28 s --- sampling period = 0.01 s)
 
 omegaA_model = lsim(sys_d1,voltage,timeVectorToPlot);
 
@@ -167,11 +167,11 @@ axis tight
 
 % transform model back to CT
 % Use 'tustin' method because sys_d1 has a pole at z=0
-sys_c1 = d2c(sys_d1, 'tustin')
-pc1 = pole(sys_c1) % poles of the CT system 
-wd1 = abs(imag(pc1(1))) 
+sys_c1 = d2c(sys_d1, 'tustin');
+pc1 = pole(sys_c1); % poles of the CT system 
+wd1 = abs(imag(pc1(1))); 
 
-[wn1,zeta1] = damp(sys_c1)
+[wn1,zeta1] = damp(sys_c1);
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -186,7 +186,8 @@ ylabel('step response [-]')
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% FIGURE 6: omegaA averaged over the periods %%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-figure(6) 
+figure(6)
+sgtitle('Velocity of wheel A averaged over the periods')
 plot(timeVectorToPlot(1:points_per_period),omegaA_mean) 
 xlabel('time [s]')
 ylabel('omegaA-mean [rad/s]')
@@ -215,10 +216,10 @@ y_ss1_model = Numc1(end)/Denc1(end); % DC gain of CT model (evaluating transfer 
 %% 2.1.4 LLS with low-pass filter applied to the input and output data
 % -------------------------------------------------------------------
 % define a low(band)-pass filter
-pd1 = pole(sys_d1)
+pd1 = pole(sys_d1);
 pc1 = log(pd1)/Ts
-interesting_frequency = (imag(pc1(3))/(2*pi))
-cutoff = 0.99*interesting_frequency  %was 1.5*interesting_frequency
+interesting_frequency = (imag(pc1(3))/(2*pi));
+cutoff = 0.99*interesting_frequency;  %was 1.5*interesting_frequency
 [B_filt,A_filt] = butter(6, cutoff/(fs/2));
 
 % apply the filter to both input and output
@@ -231,15 +232,16 @@ A = [-omegaA_filt(2:end-1), -omegaA_filt(1:end-2), voltage_filt(2:end-1), voltag
 
 % H(z) = (b1 z + b2)/(z^3 + a1 z^2 + a2 z)
 
-theta = A\b; % theta = [a1 a2 b1 b2]'
+theta_filter = A\b % theta = [a1 a2 b1 b2]'
 
-Num2 = [theta(3), theta(4)];
-Den2 = [1, theta(1) theta(2)];
+Num2 = [theta_filter(3), theta_filter(4)];
+Den2 = [1, theta_filter(1) theta_filter(2)];
 sys_d2 = tf(Num2, Den2, Ts);
 
 % plot results
 
 omegaA_model2 = lsim(sys_d2,voltage,timeVectorToPlot);
+
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -249,7 +251,7 @@ figure(7), hold on
 sgtitle('LLS with low-pass filtering')
 
 subplot(2,1,1)
-plot(t,[omegaA omegaA_model2]);
+plot(timeVectorToPlot,[omegaA omegaA_model2]);
 legend('empirical','estimated','Location','SouthWest')
 xlabel('time [s]')
 axis tight
@@ -273,163 +275,35 @@ sys_c2 = d2c(sys_d2);
 pc2 = pole(sys_c2);
 wd2 = abs(imag(pc2(2)));
 
-[wn2,zeta2]=damp(sys_c2)
+[wn2,zeta2] = damp(sys_c2);
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% FIGURE 8: step response of the CT system (filtered model) %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 figure(8)
 step(sys_d2);grid
+
+
+
 
 % relative peak value: value of response at peak/steady state value
 %y_peak = 0.00508/0.00333; %obtained from figure(8)
 %Mp = y_peak-1
-Mp_model = exp(-pi*zeta2(1)/sqrt(1-zeta2(1)^2)) %formula slide 10, C6
+Mp_model = exp(-pi*zeta2(1)/sqrt(1-zeta2(1)^2)); %formula slide 10, C6
 %Mp_measurement = 0.005081/0.003351 - 1  %obtained from figure(6)
 
 % peak time
 %tp = 0.313 %obtained from figure(8)
-tp_model = pi/wd2 %formula slide 9, C6
+tp_model = pi/wd2; %formula slide 9, C6
 %tp_measurement = 8.125 - 7.813 %obtained from figure(6)
 
 % steady state value
 [Numc2, Denc2] = tfdata(sys_c2);
 Numc2 = Numc2{1};
 Denc2 = Denc2{1};
-y_ss_model = Numc2(end)/Denc2(end) %DC gain of CT model (evaluating transfer function at s=0) 
+y_ss_model = Numc2(end)/Denc2(end); %DC gain of CT model (evaluating transfer function at s=0) 
 %y_ss = 0.00333 %obtained from figure(8)
 %y_ss_measurement = 0.003351 %obtained from figure(6)
-
-%% 2.1.4 LLS applied to the averaged output data
-% -------------------------------------------------------------------
-% collect the signals appearing in the difference equation
-b = x_mean(3:end);
-A = [-x_mean(2:end-1), -x_mean(1:end-2), F_t(2:1070-1), F_t(1:1070-2)];
-% perform the fit to get the desired parameters
-theta = A\b;
-
-% build the identified model
-Num3 = [0, theta(3), theta(4)];
-Den3 = [1, theta(1) theta(2)];
-sys_d3 = tf(Num3, Den3, Ts);
-
-x3 = lsim(sys_d3,F_t,t);
-
-% plot results
-figure(5), hold on
-sgtitle('LLS with averaging over periods')
-subplot(2,1,1)
-plot(t,[x_t x3]);
-legend('empirical','estimated','Location','SouthWest')
-xlabel('time [s]')
-axis tight
-ylabel('displacement [m]')
-subplot(2,1,2),plot(t,x_t-x3)
-legend('error')
-xlabel('time [s]')
-ylabel('displacement [m]')
-axis tight
-
-% compare all results 
-figure(6),hold on
-title('Compare all models')
-subplot(2,1,1)
-plot(t(1:1280),[x_t(1:1280) x1(1:1280) x2(1:1280), x3(1:1280)]);
-legend('empirical','model 1', 'model 2', 'model 3','Location','SouthWest')
-xlabel('time [s]')
-ylabel('displacement [m]')
-axis tight
-subplot(2,1,2),plot(t(1:1280),[x_t(1:1280)-x1(1:1280),x_t(1:1280)-x2(1:1280), x_t(1:1280)-x3(1:1280)])
-legend('error model 1', 'error model 2', 'error model 3')
-xlabel('time [s]')
-ylabel('displacement [m]')
-axis tight
-
-% in which aspects is are the new models better than the old?
-
-%% 2.1.5 Extra: Iterative Identification based on Sanathanan Koerner method 
-% -------------------------------------------------------------------------
-
-% Iteration 1
-% we start with model1.
-% Filter data using denominator of model1
-Den4 = Den1;
-Num4 = [0 0 1];
-x_filt = filtfilt(Num4, Den4, x_t); 
-F_filt = filtfilt(Num4, Den4, F_t);
-
-% Redo the parameter estimation with the filtered data
-b = x_filt(3:end);
-A = [-x_filt(2:end-1), -x_filt(1:end-2), F_filt(2:end-1), F_filt(1:end-2)];
-theta = A\b;
-Num4 = [theta(3), theta(4)];
-Den4 = [1, theta(1) theta(2)];
-
-% Iteration 2
-% Use model from the previous step to improve result.
-% Filter data using denominator of model3
-Num4 = [0 0 1];
-x_filt = filtfilt(Num4, Den4, x_t); 
-F_filt = filtfilt(Num4, Den4, F_t);
-
-% Redo the parameter estimation with the filtered data
-b = x_filt(3:end);
-A = [-x_filt(2:end-1), -x_filt(1:end-2), F_filt(2:end-1), F_filt(1:end-2)];
-theta = A\b;
-Num4 = [theta(3), theta(4)];
-Den4 = [1, theta(1) theta(2)];
-
-
-% Iteration 3 (last one)
-% Use model from the previous step to improve result.
-% Filter data using denominator of model3
-Num4 = [0 0 1];
-x_filt = filtfilt(Num4, Den4, x_t); 
-F_filt = filtfilt(Num4, Den4, F_t);
-
-% Redo the parameter estimation with the filtered data
-b = x_filt(3:end);
-A = [-x_filt(2:end-1), -x_filt(1:end-2), F_filt(2:end-1), F_filt(1:end-2)];
-theta = A\b;
-Num4 = [theta(3), theta(4)];
-Den4 = [1, theta(1) theta(2)];
-
-sys_d4 = tf(Num4, Den4, Ts);
-
-% plot results
-x4 = lsim(sys_d4,F_t,t);
-figure(7), hold on
-sgtitle('LLS after 3 iterations of the Sanathanan Koerner method')
-subplot(2,1,1)
-plot(t,[x_t x4]);
-legend('empirical','estimated','Location','SouthWest')
-xlabel('time [s]')
-axis tight
-ylabel('displacement [m]')
-subplot(2,1,2),plot(t,x_t-x4)
-legend('error')
-xlabel('time [s]')
-ylabel('displacement [m]')
-axis tight
-
-% analyse step response of model in detail: calculate rise time, settling time,
-% steady state value of the measured data (data avalaible for the
-% identification) ... and check these values for the obtained model.  
-
-
-% compare last 2 results 
-figure(8),hold on
-title('Compare last two models')
-subplot(2,1,1)
-plot(t(1:1280),[x_t(1:1280) x2(1:1280) x4(1:1280)]);
-legend('empirical','model 2 (low-pass filter)', 'model 4 (Sanathan Koerner)','Location','SouthWest')
-xlabel('time [s]')
-ylabel('displacement [m]')
-axis tight
-subplot(2,1,2),plot(t(1:1280),[x_t(1:1280)-x2(1:1280),x_t(1:1280)-x4(1:1280)])
-legend('error model 2 (low-pass filter)', 'error model 4 (Sanathan Koerner)')
-xlabel('time [s]')
-ylabel('displacement [m]')
-axis tight
-
-% in which aspects is the new model better than the low-pass filtered one or is the improvement negligible?
-
-
 
 
